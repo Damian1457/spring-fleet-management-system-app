18c7952cf3ca2389515358d0ba7c311060401f6f
Damian1457 <damianwasik.coach@gmail.com>
2023-07-07 22:28:47 +0200

    Created view for application.

---
+++ src/main/java/pl/wasik/damian/spring/app/fleet/management/system/app/FleetManagementSystemAppApplication.java
@@ -0,0 +1,13 @@
+package pl.wasik.damian.spring.app.fleet.management.system.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wasik/damian/spring/app/fleet/management/system/app/FleetManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wasik.damian.spring.app.fleet.management.system.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
