99544c62a03d33d91a875e4b29cdd32548f1fc04
Damian1457 <damianwasik.coach@gmail.com>
2023-07-17 22:24:14 +0200

    Created edit methods for country, and layouts for them.

--- src/main/java/pl/wasik/damian/spring/app/fleet/management/system/parameters/controllers/CountryController.java
+++ src/main/java/pl/wasik/damian/spring/app/fleet/management/system/parameters/controllers/CountryController.java
@@ -28,17 +28,35 @@ public class CountryController {
 
     @GetMapping("/countryAdd")
     public String addCountry() {
+        LOGGER.info("addCountry()");
         return "parameters/countryAdd";
     }
 
     @PostMapping("/countries")
     public String save(Country country) {
+        LOGGER.info("save()");
+        countryService.save(country);
+        return "redirect:/countries";
+    }
+
+    @GetMapping("/countryEdit/{id}")
+    public String editCountry(@PathVariable Long id, Model model) {
+        LOGGER.info("editCountry()");
+        Country country = countryService.getById(id);
+        model.addAttribute("country", country);
+        return "parameters/countryEdit";
+    }
+
+    @PostMapping("/countries/update/{id}")
+    public String edit(Country country) {
+        LOGGER.info("edit()");
         countryService.save(country);
         return "redirect:/countries";
     }
 
     @GetMapping("/countries/delete/{id}")
     public String delete(@PathVariable Long id) {
+        LOGGER.info("delete()");
         countryService.delete(id);
         return "redirect:/countries";
     }
