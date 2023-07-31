03feeb589cdc6bb5facafc70cc730127d548e1ff
Damian1457 <damianwasik.coach@gmail.com>
2023-07-31 18:31:48 +0200

    Created tests for Country Repository.

--- src/main/java/pl/wasik/damian/spring/app/fleet/management/system/parameters/models/Country.java
+++ src/main/java/pl/wasik/damian/spring/app/fleet/management/system/parameters/models/Country.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,8 +13,9 @@ import java.util.List;
 
 @Entity
 @Data
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Country {
 
